62190ebe9f60d0191a085c69658cda85d4e7a167
Jeeyoun-S <109322034+Jeeyoun-S@users.noreply.github.com>
2023-07-15 23:44:58 +0900

    [level 3] Title: 등굣길, Time: 4.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/README.md
+++ 프로그래머스/lv3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 5.95 ms
+메모리: 10.3 MB, 시간: 4.26 ms
 
 ### 구분
 
--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,21 +1,19 @@
 from collections import deque
 
 def solution(m, n, puddles):
-
-    delta = ((-1, 0), (0, -1))
-    maps = [[0 for j in range(m)] for i in range(n)]
-    maps[0][0] = 1
     
+    delta = ((-1, 0), (0, -1)) # 이동할 위치
+    maps = [[0 for j in range(m)] for i in range(n)] # 지도
+    maps[0][0] = 1 # 집에서 출발
+    
+    # 1행 1열 -> 1행 2열 -> ... -> 2행 1열 순으로 반복
     for x in range(n):
         for y in range(m):
-            if [y+1, x+1] in puddles:
-                maps[x][y] = 0
+            if [y+1, x+1] in puddles: # 웅덩이가 있다면
                 continue
+            # 위와 왼쪽에 값이 있다면 더해주기
             for dx, dy in delta:
                 xx, yy = x+dx, y+dy
                 if 0 <= xx < n and 0 <= yy < m:
                     maps[x][y] = (maps[x][y] + maps[xx][yy]) % 1000000007
-            # print(x, y, maps)
-                
-    print(maps)
     return maps[n-1][m-1]
