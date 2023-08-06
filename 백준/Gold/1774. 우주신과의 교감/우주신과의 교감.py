0e449fe0ce82d6a5e8b3302f1f79a252a1c6220a
Jeeyoun Seo <109322034+Jeeyoun-S@users.noreply.github.com>
2023-08-06 17:26:09 +0900

    [Gold III] Title: 우주신과의 교감, Time: 668 ms, Memory: 109076 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
@@ -1,49 +1,50 @@
+from heapq import heappop, heappush
 import sys
-import heapq
+
+
+def find(x):
+    if home[x] < 0:
+        return x
+
+    home[x] = find(home[x])
+    return home[x]
+
+
+def union(a, b):
+    a, b = find(a), find(b)
+    home[b] = a
+
 
 N, M = map(int, sys.stdin.readline().split())
-parents = [i for i in range(N)]
-locations = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
-heap, total = [], 0
+home = [-1] * (N + 1)
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+my_heap = []
+answer, cnt = 0, 0
 
-def find_parents(x):
-    if parents[x] != x:
-        parents[x] = find_parents(parents[x])
-    return parents[x]
-
-def union(x, y):
-    if x > y:
-        parents[x] = y
-    else:
-        parents[y] = x
-
-for _ in range(M):
-    X, Y = map(int, sys.stdin.readline().split())
-    p_x, p_y = find_parents(X-1), find_parents(Y-1)
-    if p_x != p_y:
-        union(p_x, p_y)
-
-# print(parents)
-for i in range(N-1):
+for i in range(N):
     for j in range(i+1, N):
-        ix, iy = locations[i]
-        jx, jy = locations[j]
-        cost = ((ix - jx)**2 + (iy - jy)**2)**(1/2)
-        heapq.heappush(heap, (cost, i, j))
+        weight = pow(pow(arr[i][0] - arr[j][0], 2) + pow(arr[i][1] - arr[j][1], 2), 0.5)
+        heappush(my_heap, (weight, i+1, j+1))
 
-# print(heap)
-# print(parents)
-# print(visited)
+# 이제 주어지는 좌표들은 연결된 좌표이므로 연결시켜주기
+for _ in range(M):
+    x, y = map(int, sys.stdin.readline().split())
+    if find(x) != find(y):
+        union(x, y)
+        cnt += 1
 
-while heap:
-    cost, x, y = heapq.heappop(heap)
-    # print(cost, x, y)
-    p_x, p_y = find_parents(x), find_parents(y)
-    if p_x != p_y:
-        union(p_x, p_y)
-        total += cost
+for i in range(len(my_heap)):
+	# 우선순위 큐인 힙에서 최소값 하나씩 뽑아
+    w, a, b = heappop(my_heap)
+	# 해당 좌표들인 a와 b가 연결X 일 경우,
+    if find(a) != find(b):
+    	# 연결시켜주고 가중치 저장
+        union(a, b)
+        answer += w
+        cnt += 1
+        
+		# N개의 정점을 가진 그래프의 최소 간선은 N-1개
+        if cnt == N-1:
+            break
 
-print('%.2f' % total)
-# print(heap)
-# print(parents)
-# print(locations)
+print(format(answer, ".2f"))
