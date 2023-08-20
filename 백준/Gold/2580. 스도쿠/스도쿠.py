9ea550a532cb666cbf27f45bf3342b56ec1fe5b8
Jeeyoun Seo <109322034+Jeeyoun-S@users.noreply.github.com>
2023-08-20 22:19:02 +0900

    [Gold IV] Title: 스도쿠, Time: 3516 ms, Memory: 144880 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,41 +1,42 @@
-def row(a, n): # 가로
-    for i in range(9):
-        if n == sudoku[a][i]: # 이미 있으면
-            return False
-    return True
-
-def column(a, n): # 세로
-    for i in range(9):
-        if n == sudoku[i][a]: # 이미 있으면
-            return False
-    return True
-
-def square(y, x, n): # 3x3 칸
-    for i in range(3):
-        for j in range(3):
-            if n == sudoku[y//3 * 3 + i][x//3 * 3 + j]: # 칸내에 이미 있으면
-                return False
-    return True
-
-def find(n):
-    if n == len(blank): # 빈 공간 만큼 사용했으면
-        for i in sudoku: # 출력 후
-            print(*i) 
-        exit() # 강제 종료
-
-    for i in range(1,10):
-        y = blank[n][0]
-        x = blank[n][1]
-        if column(x,i) and row(y,i) and square(y, x, i):
-            sudoku[y][x] = i
-            find(n+1)
-            sudoku[y][x] = 0
-
 import sys
-sudoku = [list(map(int,sys.stdin.readline().split())) for _ in range(9)]
-blank = []
+
+sudoku = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
+empty = []
 for i in range(9):
     for j in range(9):
         if sudoku[i][j] == 0:
-            blank.append([i,j])
-find(0)
+            empty.append([i, j])
+
+def find_row(row, n):
+    for col in range(9):
+        if n == sudoku[row][col]:
+            return False
+    return True
+
+def find_column(col, n):
+    for row in range(9):
+        if n == sudoku[row][col]:
+            return False
+    return True
+
+def find_square(row, col, n):
+    for i in range(3):
+        for j in range(3):
+            if n == sudoku[row//3 * 3 + i][col//3 * 3 + j]:
+                return False
+    return True
+
+def backtracking(n):
+    if n == len(empty):
+        for s in sudoku:
+            print(*s)
+        exit()
+
+    for i in range(1, 10):
+        x, y = empty[n][0], empty[n][1]
+        if find_row(x, i) and find_column(y, i) and find_square(x, y, i):
+            sudoku[x][y] = i
+            backtracking(n+1)
+            sudoku[x][y] = 0
+
+backtracking(0)
